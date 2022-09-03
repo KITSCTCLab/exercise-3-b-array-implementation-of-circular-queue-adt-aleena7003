305f1d163944f4374a3f870a8ef5484cfc91ee10
aleena7003 <109643956+aleena7003@users.noreply.github.com>
2022-09-03 22:34:01 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,103 +1,51 @@
 class MyCircularQueue:
-    def init(self, size: int):
-        self.size=size
-        self.queue=[None]*size
-        self.rear=-1
-        self.front=-1
-    def __init__(self, n: int):
-        self.n = n
-        self.array = [None] * self.n
-        self.front = 0  # index of the first element
-        self.rear = 0
-        self.size = 0
+    def __init__(self, size: int):
+        # Write code here
+        self.queue = [0] * size
+        self.size = size
+        self.front, self.rear = -1, -1
 
-    def __len__(self) -> int:
-        """
-        >>> cq = CircularQueue(5)
-        >>> len(cq)
-        0
-        >>> cq.enqueue("A")  # doctest: +ELLIPSIS
-        <data_structures.queue.circular_queue.CircularQueue object at ...
-        >>> len(cq)
-        1
-        """
-        return self.size
+    def enqueue(self, value: int) -> bool:
+        # Write code here
+        if self.is_full():
+            return False
+        if self.front == -1:
+            self.front, self.rear = 0, 0
+        else:
+            self.rear = (self.rear + 1) % self.size
+        self.queue[self.rear] = value
+        return True
 
-    def is_empty(self) -> bool:
-        """
-        >>> cq = CircularQueue(5)
-        >>> cq.is_empty()
-        True
-        >>> cq.enqueue("A").is_empty()
-        False
-        """
-        return self.size == 0
 
-    def first(self):
-        """
-        >>> cq = CircularQueue(5)
-        >>> cq.first()
-        False
-        >>> cq.enqueue("A").first()
-        'A'
-        """
-        return False if self.is_empty() else self.array[self.front]
+    def dequeue(self) -> bool:
+        # Write code here
+        if self.is_empty(): 
+            return False
+        if self.front == self.rear:
+            self.front, self.rear = -1, -1
+        else:
+            self.front = (self.front + 1) % self.size
+        return True
 
-    def enqueue(self, data):
-        """
-        This function insert an element in the queue using self.rear value as an index
-        >>> cq = CircularQueue(5)
-        >>> cq.enqueue("A")  # doctest: +ELLIPSIS
-        <data_structures.queue.circular_queue.CircularQueue object at ...
-        >>> (cq.size, cq.first())
-        (1, 'A')
-        >>> cq.enqueue("B")  # doctest: +ELLIPSIS
-        <data_structures.queue.circular_queue.CircularQueue object at ...
-        >>> (cq.size, cq.first())
-        (2, 'A')
-        """
-        if self.size >= self.n:
-            raise Exception("QUEUE IS FULL")
+    def get_front(self) -> int:
+        # Write code here
+        if not self.is_empty():
+            return self.queue[self.front]
+        return -1
 
-        self.array[self.rear] = data
-        self.rear = (self.rear + 1) % self.n
-        self.size += 1
-        return self
-
-    def dequeue(self):
-        """
-        This function removes an element from the queue using on self.front value as an
-        index
-        >>> cq = CircularQueue(5)
-        >>> cq.dequeue()
-        Traceback (most recent call last):
-           ...
-        Exception: UNDERFLOW
-        >>> cq.enqueue("A").enqueue("B").dequeue()
-        'A'
-        >>> (cq.size, cq.first())
-        (1, 'B')
-        >>> cq.dequeue()
-        'B'
-        >>> cq.dequeue()
-        Traceback (most recent call last):
-           ...
-        Exception: UNDERFLOW
-        """
-        if self.size == 0:
-            raise Exception("UNDERFLOW")
-
-        temp = self.array[self.front]
-        self.array[self.front] = None
-        self.front = (self.front + 1) % self.n
-        self.size -= 1
-        return temp
+    def get_rear(self):
+        # Write code here
+        if not self.is_empty():
+            return self.queue[self.rear]
+        return -1
     
-               
+    def is_empty(self):
+        # Write code here
+        return self.front == -1
 
-  
-           
-
+    def is_full(self):
+        # Write code here
+        return (self.front == 0 and self.rear == (self.size - 1)) or (self.front == (self.rear + 1) % self.size)
 
 # Do not change the following code
 operations = []
@@ -129,3 +77,4 @@ for i in range(len(operations)):
         result.append(obj.is_empty())
 
 print(result)
+
